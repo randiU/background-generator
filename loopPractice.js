0b17c650934a65e9172c0f268b02a8c649c8f6c6
randiU <33576756+randiU@users.noreply.github.com>
2019-02-21 10:30:55 -0700

    initial commit

---
+++ loopPractice.js
@@ -0,0 +1,27 @@
+let todos = [
+"clean room",
+"brush teeth",
+"exercise"
+];
+
+for ( var i=0; i < todos.length; i++) {
+	todos.pop();
+	console.log(todos);
+}
+
+
+
+var counterOne = 0;
+
+while (counterOne <= 10) {
+	console.log(counterOne);
+	counterOne++;
+}
+
+var counterTwo = 10;
+
+do {
+	console.log(counterTwo);
+	counterTwo--;
+} while (counterTwo > 0);
+
---
+++ scriptTern.js
@@ -0,0 +1,21 @@
+
+
+const obj = {
+	player: 'bobby',
+	experience: 100,
+	wizardLevel: false
+}
+
+const { player, wizardLevel } = obj;
+
+console.log(player, wizardLevel )
+
+
+const a = 'blue';
+const b = 'yellow';
+const c = {};
+
+
+const monkey = { a, b, c};
+
+console.log(monkey.a);
